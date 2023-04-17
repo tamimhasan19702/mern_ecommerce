f62a5313d59738afa4879ae872a8695de66eee1f
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-18 02:55:38 +0600

    localhost hello server running successfully on postman

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -5,6 +5,12 @@ const app = express();
 
 env.config();
 
+app.get('/', (req,res,next) => {
+    res.status(200).json({
+        message: "Hello from the server"
+    });
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on the port: ${process.env.PORT}`)
 })
