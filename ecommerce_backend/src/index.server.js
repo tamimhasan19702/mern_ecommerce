74356e9d42a991f99fb0745ddec5ebc7ce15bef8
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-18 03:10:07 +0600

    setting up body parser to the post request

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const env = require('dotenv')
-
 const app = express();
+const bodyParser = require('body-parser');
+
 
 env.config();
+
+app.use(bodyParser());
+
 
 app.get('/', (req,res,next) => {
     res.status(200).json({
         message: "Hello from the server"
+    });
+})
+
+app.post('/data', (req,res,next) => {
+    res.status(200).json({
+        message: req.body
     });
 })
 
