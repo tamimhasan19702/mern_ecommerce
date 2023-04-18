1cce8797960e51d7210d4b2aec063fba1645b806
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-18 22:49:18 +0600

    mongoDb database with mongoose error fixed with bad auth

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -1,26 +1,36 @@
 const express = require("express");
-const env = require('dotenv')
+const dotenv = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+dotenv.config(); // Return environment variables
 
-env.config();
+// Add error handling for Mongoose connection
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(() => {
+  console.log('Database Connected');
+});
 
-app.use(bodyParser());
-
+app.use(bodyParser.json()); // Call json() function of bodyParser to parse JSON data
 
 app.get('/', (req,res,next) => {
-    res.status(200).json({
-        message: "Hello from the server"
-    });
-})
+  res.status(200).json({
+    message: "Hello from the server"
+  });
+});
 
 app.post('/data', (req,res,next) => {
-    res.status(200).json({
-        message: req.body
-    });
-})
+  res.status(200).json({
+    message: req.body
+  });
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is running on the port: ${process.env.PORT}`)
-})
+  console.log(`server is running on the port: ${process.env.PORT}`);
+});
