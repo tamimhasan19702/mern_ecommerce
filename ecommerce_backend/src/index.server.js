3277d63dbb394cd8c21d1f715539a5785f104424
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-18 23:06:23 +0600

    user routes created and added to the server

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -1,36 +1,36 @@
+//First, the program imports required packages such as express, dotenv, body-parser and mongoose.
 const express = require("express");
 const dotenv = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//routes
+const userRoutes = require('./routes/user')
+
+
 dotenv.config(); // Return environment variables
 
-// Add error handling for Mongoose connection
+// The mongoose.connect() function is used to connect to the MongoDB database. The database URL is constructed using three environment variables: DB_USER, DB_PASSWORD, and DB_DATABASE.
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 ).then(() => {
   console.log('Database Connected');
+}).catch(() => {
+    console.log('Database Connection Failed')
 });
 
-app.use(bodyParser.json()); // Call json() function of bodyParser to parse JSON data
+app.use(bodyParser.json()); // The body-parser middleware is used to parse JSON data sent in request bodies. 
 
-app.get('/', (req,res,next) => {
-  res.status(200).json({
-    message: "Hello from the server"
-  });
-});
 
-app.post('/data', (req,res,next) => {
-  res.status(200).json({
-    message: req.body
-  });
-});
 
+
+
+//Finally, the server listens on the port specified by the PORT environment variable. When the server starts, a message is logged to the console indicating that the server is running on the specified port.
 app.listen(process.env.PORT, () => {
   console.log(`server is running on the port: ${process.env.PORT}`);
 });
---
+++ ecommerce_backend/src/routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router()
+
+
+router.post('/signin',(req,res) => {
+    
+})
+
+router.get('/signup',(req,res) => {
+
+})
+
+
+
+
+module.exports = router;
