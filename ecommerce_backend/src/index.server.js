f2f3ab22a5c91f3037785547eae2663b7d94da97
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-20 00:45:08 +0600

    user modal scheme updates

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -1,4 +1,4 @@
-//First, the program imports required packages such as express, dotenv, body-parser and mongoose.
+
 const express = require("express");
 const dotenv = require('dotenv');
 const app = express();
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/user')
 
 dotenv.config(); // Return environment variables
 
-// The mongoose.connect() function is used to connect to the MongoDB database. The database URL is constructed using three environment variables: DB_USER, DB_PASSWORD, and DB_DATABASE.
+
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
   {
@@ -24,13 +24,9 @@ mongoose.connect(
     console.log('Database Connection Failed')
 });
 
-app.use(bodyParser.json()); // The body-parser middleware is used to parse JSON data sent in request bodies. 
+app.use(bodyParser.json()); 
+app.use('/api',userRoutes);
 
-
-
-
-
-//Finally, the server listens on the port specified by the PORT environment variable. When the server starts, a message is logged to the console indicating that the server is running on the specified port.
 app.listen(process.env.PORT, () => {
   console.log(`server is running on the port: ${process.env.PORT}`);
 });
---
+++ ecommerce_backend/src/modals/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    LastName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    email: {
+        type:
+    }
+})
+
+module.exports = mongoose.model("User");
