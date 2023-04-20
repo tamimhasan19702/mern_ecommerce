5532bf5477665d73279e4a0334e7a927f0bef57b
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-20 20:32:24 +0600

    rolled back to previous code

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -4,11 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-dotenv.config(); // Return environment variables
+dotenv.config(); 
 
-// Add error handling for Mongoose connection
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -18,6 +17,7 @@ mongoose.connect(
 });
 
 app.use(bodyParser.json()); // Call json() function of bodyParser to parse JSON data
+
 
 app.get('/', (req,res,next) => {
   res.status(200).json({
