3e39333ef6db9e429ffb14b923984ee57847316d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-20 22:00:40 +0600

    model user.js updates

--- ecommerce_backend/src/index.server.js
+++ ecommerce_backend/src/index.server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const env = require('dotenv');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-dotenv.config(); 
+//routes
+const userRoutes = require('./routes/user');
+
+env.config(); 
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3a3gnjl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`,
@@ -16,20 +19,10 @@ mongoose.connect(
   console.log('Database Connected');
 });
 
-app.use(bodyParser.json()); // Call json() function of bodyParser to parse JSON data
+app.use(bodyParser()); // Call json() function of bodyParser to parse JSON data
+app.use('/api',userRoutes);
 
 
-app.get('/', (req,res,next) => {
-  res.status(200).json({
-    message: "Hello from the server"
-  });
-});
-
-app.post('/data', (req,res,next) => {
-  res.status(200).json({
-    message: req.body
-  });
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on the port: ${process.env.PORT}`);
