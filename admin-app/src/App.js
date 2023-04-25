b60922604960c22a9e167ca24b0ee0613201faf1
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-26 02:53:12 +0600

    Header component added to the layout

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <div class="container-fluid bg-light text-dark p-5">
-      <div class="container bg-light p-5">
-      <h1 class="display-4 fw-bold">Welcome to Admin Dashboard</h1>
-      </div>
-    </div>
+      
+
     </div>
   );
 }
