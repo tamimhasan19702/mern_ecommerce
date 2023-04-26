4b90c6026af866a60697fda2c6fac79e47d9d9ba
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-27 01:56:51 +0600

    Sign in form updated

--- admin-app/src/containers/Signin/Signin.js
+++ admin-app/src/containers/Signin/Signin.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import { Form,Button,Container,Row,Col } from "react-bootstrap";
 
 export default function Signin() {
   return (
     <Layout>
-      signin
+      <Container>
+        <Row style={{marginTop: '50px'}}>
+          <Col md={{span: 6, offset: 3 }}>
+            <Form >
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
 }
