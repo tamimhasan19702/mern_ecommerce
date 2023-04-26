afb0136daad759a6fe0f5075903902ee77bd6ef4
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-26 14:01:11 +0600

    admin router setup done

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css';
-import Layout from './components/Layout/Layout';
-
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Home from './containers/Home/Home';
+import Signin from './containers/Signin/Signin';
+import Signup from './containers/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
