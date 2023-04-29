391b29179c313d6bd961f0976b3521026e083600
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-29 21:46:12 +0600

    test commit

--- ecommerce_backend/src/routes/auth.js
+++ ecommerce_backend/src/routes/auth.js
@@ -5,9 +5,6 @@ const router = express.Router();
 router.post('/signup',signup);
 router.post('/signin',signin);
 
-// router.post('/profile',requireSignin,(req,res) => {
-//     res.status(200).json({ user: 'profile' })
-// });
 
 
 module.exports = router;
