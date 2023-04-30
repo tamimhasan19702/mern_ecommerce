fe2cc96fa827ec7e3c40203e5b0296ca150eabb6
tamimhasan19702 <tamimhasancu@gmail.com>
2023-04-30 16:29:03 +0600

    categorie routes created

---
+++ ecommerce_backend/src/routes/categories.js
@@ -0,0 +1,28 @@
+/** @format */
+
+const express = require("express");
+const router = express.Router();
+const Category = require("../models/categories");
+const slugify = requrie("slugify");
+
+router.post("/category/create", (req, res) => {
+  const categoryObj = {
+    name: req.body.name,
+    slug: slugify(req.body.name),
+  };
+
+  if (req.body.parentId) {
+    categoryObj.parentId = req.body.parentId;
+  }
+
+  const cat = new Category(categoryObj);
+  cat.save((error, category) => {
+    if (error) return res.status(400).json({ error });
+
+    if (category) {
+      return res.status(201).json({ category });
+    }
+  });
+});
+
+module.exports = router;
