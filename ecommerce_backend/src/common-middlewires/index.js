06b033d133ba0f2564b631ab36e2e1ee58ddf7e0
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-02 14:20:39 +0600

    admin route issue fixed

--- ecommerce_backend/src/common-middlewires/index.js
+++ ecommerce_backend/src/common-middlewires/index.js
@@ -1,20 +1,27 @@
 /** @format */
+const jwt = require('jsonwebtoken')
 
 //require sign-in
 
-const requireSignin = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-  const user = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = user;
-  next();
+exports.requireSignin = (req, res, next) => {
+  
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  }
+  return res.status(400).json({ message: 'Authorization required' });
+
 };
+
 
 exports.userMiddleware = (req, res, next) => {};
 
 
 exports.adminMiddleware = (req, res, next) => {
-  if(req.user.role !== 'admin'){
-    return res.status(400).json({ message: 'Access denied!'})
+  if (req.user.role !== "admin") {
+    return res.status(400).json({ message: "Access denied!" });
   }
   next();
 };
--- ecommerce_backend/src/routes/categories.js
+++ ecommerce_backend/src/routes/categories.js
@@ -4,8 +4,9 @@ const express = require("express");
 const router = express.Router();
 
 const { addCategory, getCategories } = require("../controller/category");
+const {requireSignin,adminMiddleware} = require('../common-middlewires/index')
 
-router.post("/category/create", addCategory);
+router.post("/category/create",requireSignin,adminMiddleware,addCategory);
 router.get("/category/getcategories", getCategories);
 
 module.exports = router;
