adf98c86b1a9ec10f40f33744695920c6ab84b31
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-02 14:05:39 +0600

    updates on adminMiddleware

--- ecommerce_backend/src/common-middlewires/index.js
+++ ecommerce_backend/src/common-middlewires/index.js
@@ -1,8 +1,20 @@
+/** @format */
+
 //require sign-in
 
-const requireSignin = (req,res,next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user ;
-    next();
-}
+const requireSignin = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+};
+
+exports.userMiddleware = (req, res, next) => {};
+
+
+exports.adminMiddleware = (req, res, next) => {
+  if(req.user.role !== 'admin'){
+    return res.status(400).json({ message: 'Access denied!'})
+  }
+  next();
+};
