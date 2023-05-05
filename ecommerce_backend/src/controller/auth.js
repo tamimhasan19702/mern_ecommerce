3a07b895171441fd8658998747fdd32d3711d0f6
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-06 00:01:25 +0600

    validation response and middleware successfully applied

--- ecommerce_backend/src/controller/auth.js
+++ ecommerce_backend/src/controller/auth.js
@@ -7,8 +7,6 @@ const { validationResult } = require('express-validator');
 
 exports.signup = (req, res) => {
 
-  const errors = validationResult(req);
-  return res.status(400).json({ errors: errors.array() })
 
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (user)
--- ecommerce_backend/src/routes/auth.js
+++ ecommerce_backend/src/routes/auth.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const { signup, signin } = require('../controller/auth');
 const router = express.Router();
-const { check } = require('express-validator')
+const { validationRequest, isRequestValidated } = require('../validators/auth');
 
-router.post('/signup',[
-            check('firstName')
-            .notEmpty()
-            .withMessage('firstName is required'),
-            check('lastName')
-            .notEmpty()
-            .withMessage('lastName is required'),
-            check('email')
-            .isEmail()
-            .withMessage('Valid Email is required'),
-            check('password')
-            .isLength({ min: 6 })
-            .withMessage('Password must be at least 6 character long')
-            ], signup);
+router.post('/signup', validationRequest ,isRequestValidated,signup);
 router.post('/signin',signin);
 
 
