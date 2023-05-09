0658a1750f4e766ba5d1e5f004a1dc78c32c1c66
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-09 23:59:13 +0600

    image file fixed in multer

--- ecommerce_backend/src/routes/product.js
+++ ecommerce_backend/src/routes/product.js
@@ -1,25 +1,32 @@
 /** @format */
 
 const express = require("express");
-const {requireSignin,adminMiddleware} = require("../common-middlewires");
-const {createProduct} = require('../controller/product');
-const multer = require('multer');
+const { requireSignin, adminMiddleware } = require("../common-middlewires");
+const { createProduct } = require("../controller/product");
+const multer = require("multer");
 const router = express.Router();
-const shortId = require('shortid');
-const path = require('path');
+const shortId = require("shortid");
+const path = require("path");
 
 const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, path.join(path.dirname(__dirname), "uploads"));
-        },
-        filename: function (req, file, cb) {
-          cb(null, shortId.generate() + "-" + file.originalname);
-        },
-      });
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortId.generate() + "-" + file.originalname);
+  },
+});
 
-const upload = multer({storage: storage})
+const upload = multer({
+  storage: storage,
+});
 
-        router.post('/product/create',requireSignin,adminMiddleware,upload.array('productPicture'),createProduct);
+router.post(
+  "/product/create",
+  requireSignin,
+  adminMiddleware,
+  upload.single("productPicture"),
+  createProduct
+);
 
-        
 module.exports = router;
