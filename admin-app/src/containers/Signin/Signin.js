962a1013c7bff5917cf95240a0860c80cc407c2b
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-19 12:11:55 +0600

    authentication action added to the sign up button

--- admin-app/src/containers/Signin/Signin.js
+++ admin-app/src/containers/Signin/Signin.js
@@ -1,35 +1,48 @@
+/** @format */
+
 import React from "react";
 import Layout from "../../components/Layout/Layout";
-import { Form,Button,Container,Row,Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import Input from "../../components/Ui/input/Input";
+import { login } from "../../actions";
 
 export default function Signin() {
+
+   const userLogin = () => {
+    const user = {
+      email: 'User@gmail.com',
+      password: '1234567890'
+    }
+    login(user)
+   }
+
+
   return (
     <Layout>
       <Container>
-        <Row style={{marginTop: '50px'}}>
-          <Col md={{span: 6, offset: 3 }}>
-            <Form >
-            <Input 
-            label="Email"
-            placeholder="Email"
-            value=""
-            type="email"
-            onChange={() => {}}
-            />
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form onSubmit={userLogin}> 
+              <Input
+                label="Email"
+                placeholder="Email"
+                value=""
+                type="email"
+                onChange={() => {}}
+              />
 
-            <Input 
-            label="Password"
-            placeholder="Password"
-            value=""
-            type="password"
-            onChange={() => {}}
-            />
-            
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+              <Input
+                label="Password"
+                placeholder="Password"
+                value=""
+                type="password"
+                onChange={() => {}}
+              />
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
           </Col>
         </Row>
       </Container>
