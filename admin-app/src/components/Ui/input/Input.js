4a4356ff9a1cd119063b45c8e22960e3b9cef057
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-27 22:05:21 +0600

    updates on ui input component

--- admin-app/src/components/Ui/input/Input.js
+++ admin-app/src/components/Ui/input/Input.js
@@ -1,17 +1,42 @@
+/** @format */
+
 import React from "react";
 import { Form } from "react-bootstrap";
 
-export default function Input(props) {
-  return (
-        <Form.Group className="mb-3" controlId={`formBasic${props.label}`}>
-                <Form.Label>{props.label}</Form.Label>
-                <Form.Control 
-                type={props.type} placeholder={props.placeholder}
-                value={props.value}
-                onChange={props.change} />
-                <Form.Text className="text-muted">
-                {props.errorMessage}
-                </Form.Text>
-        </Form.Group>
-  );
+const Input = (props) => {
+
+  let input = null;
+
+  switch(props.type){
+    case 'select':
+    input = <Form.Group>
+            {props.label && <Form.Label>{props.label}</Form.Label>}
+            <select
+            className="form-control form-control-sm"
+            value={props.value}
+            onChange={props.onChange}>
+
+              <option value="">{props.placeholder}</option>
+            
+              {
+                props.options.length > 0 ?
+                props.options.map((option,index) => {
+                  <option key={index} value={option.value}>{option.name}</option>
+                }) : null
+              }
+
+            </select>       
+            </Form.Group>
+            break;
+            case 'text':
+            default:
+              input = <Form.Group>
+                       {ptops.label && <Form.Label>{props.label}</Form.Label>}
+                      </Form.Group>
+  }
+
+  
 }
+
+
+export default Input
