8fff230e55cd24f856b8b7157710c5c8248f4b39
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-28 21:18:39 +0600

    user authentication setup done

--- admin-app/src/actions/auth.actions.js
+++ admin-app/src/actions/auth.actions.js
@@ -7,9 +7,22 @@ export const login = (user) => {
     
     return async (dispatch) => {
 
+        dispatch({ type: authConstants.LOGIN_REQUEST })
         const res = await axios.post(`/admin/signin`,{
         ...user    
         })
+
+        if(res.status === 200){
+            const {token,user} = res.data;
+            localStorage.setItem('token',token);
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token,user
+                }
+            });
+        }
+
 
         dispatch({
             type: authConstants.LOGIN_REQUEST,
