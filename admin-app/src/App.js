1f4c8d9a9458fba41a17f6003afd351e1d124544
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-29 00:21:16 +0600

    Private Route added to the Home Page

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -6,13 +6,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Signin from "./containers/Signin/Signin";
 import Signup from "./containers/Signup/Signup";
+import PrivateRoute from "./components/HOC/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <PrivateRoute exact path="/" component={Home} />
           <Route path="/signin" component={Signin} />
           <Route path="/signup" component={Signup} />
         </Switch>
