ff620d997cb91e55f61d194ebb212bd84b414009
tamimhasan19702 <tamimhasancu@gmail.com>
2023-05-30 21:18:26 +0600

    user authentication stored in the browser

--- admin-app/src/containers/Signin/Signin.js
+++ admin-app/src/containers/Signin/Signin.js
@@ -1,68 +1,72 @@
-import React, { useState } from "react";
+/** @format */
+
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import Input from "../../components/Ui/input/Input";
-import { login } from "../../actions";
-import { useDispatch, useSelector } from 'react-redux';
+import { isUserLoggedIn, login } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
-function Signin (props) {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const auth = useSelector(state => state.auth)
-
+function Signin(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const auth = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(!auth.authenticate){
+      dispatch(isUserLoggedIn());
+    }
+  },[])
+
 
   const userLogin = async (e) => {
     e.preventDefault();
 
+    const user = {
+      email: email,
+      password: password,
+    };
 
-      const user = {
-        email: email,
-        password: password
-      };
-  
-       dispatch(login(user));
+    dispatch(login(user));
+  };
 
-   }
-
-  if(auth.authenticate){
-    return <Redirect to={'/'}/>
+  if (auth.authenticate) {
+    return <Redirect to={"/"} />;
   }
-
 
   return (
     <Layout>
-        <Container>
-            <Row style={{ marginTop: '50px' }}>
-                <Col md={{span: 6, offset: 3}}>
-                    <Form onSubmit={userLogin}>
-                        <Input 
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="email"
-                            label="Email"
-                            placeholder="Email"
-                            value={email}
-                        />
+      <Container>
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form onSubmit={userLogin}>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                label="Email"
+                placeholder="Email"
+                value={email}
+              />
 
-                        <Input 
-                            onChange={(e) => setPassword(e.target.value)}
-                            label="Password"
-                            placeholder="Password"
-                            value={password}
-                            type="password"
-                        />
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-            {error && <p>{error}</p>}
-        </Container>
+              <Input
+                onChange={(e) => setPassword(e.target.value)}
+                label="Password"
+                placeholder="Password"
+                value={password}
+                type="password"
+              />
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        {error && <p>{error}</p>}
+      </Container>
     </Layout>
   );
 }
