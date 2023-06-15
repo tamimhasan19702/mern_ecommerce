85de3351bb377e02870f1a56d7c57be8b10c46bf
tamimhasan19702 <tamimhasancu@gmail.com>
2023-06-15 21:24:48 +0600

    category api successfully called by admin frontend

---
+++ admin-app/src/actions/category.action.js
@@ -0,0 +1,11 @@
+import axios from "../helpers/axios";
+
+export const getAllCategory = () => {
+    return async dispatch => {
+        const res = await axios.get(`category/getcategory`);
+        console.log(res)
+
+
+
+    }
+}
--- admin-app/src/containers/category/Category.js
+++ admin-app/src/containers/category/Category.js
@@ -1,8 +1,19 @@
-import React from "react";
-import Layout from "../../components/Layout/Layout";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { getAllCategory } from "../../actions";
+import Layout from "../../components/Layout/Layout";
 
 export default function Category() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCategory())
+  
+  
+  },[])
+
   return (
     <Layout sidebar>
         <Container>
