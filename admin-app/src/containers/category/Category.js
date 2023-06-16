38451fc2d14b9b49700326347afd1fddde9af448
tamimhasan19702 <tamimhasancu@gmail.com>
2023-06-16 22:27:08 +0600

    displayed all the category item to the menu list

--- admin-app/src/containers/category/Category.js
+++ admin-app/src/containers/category/Category.js
@@ -1,18 +1,34 @@
 import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../../actions";
 import Layout from "../../components/Layout/Layout";
 
 export default function Category() {
 
+  const category = useSelector(state => state.category);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCategory())
   
   
-  },[])
+  },[]);
+
+  const renderCategories = (categories) => {
+
+    let myCategories = [];
+    for (let category of categories){
+      myCategories.push(
+        <li>
+          {category.name}
+        </li>
+      )
+    }
+
+    return myCategories;
+
+  }
 
   return (
     <Layout sidebar>
@@ -27,7 +43,9 @@ export default function Category() {
             </Row>
             <Row>
               <Col md={12}>
-              
+              <ul>
+                {renderCategories(category.categories)}
+              </ul>
               </Col>
             </Row>
         </Container>
