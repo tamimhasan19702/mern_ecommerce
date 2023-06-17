54778b119a4f754c39785ebd3a4472a178c68891
tamimhasan19702 <tamimhasancu@gmail.com>
2023-06-17 20:32:29 +0600

    addCategory action finished

--- admin-app/src/actions/category.action.js
+++ admin-app/src/actions/category.action.js
@@ -27,7 +27,18 @@ export const getAllCategory = () => {
 
 export const addCategory = (form) => {
     return async dispatch => {
+        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST});
         const res = await axios.post(`/category/create`,form);
-        console.log(res)
+        if(res.status === 201){
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+                payload: res.data.category
+            });
+        } else{
+            dispatch({
+                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+                payload: res.data.error
+            });
+        }
     }
 }
