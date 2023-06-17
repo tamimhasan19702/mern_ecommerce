fe7a3b8de7a2bbe37bb1919d26878f4e2ac2cab0
tamimhasan19702 <tamimhasancu@gmail.com>
2023-06-17 19:39:51 +0600

    modal bootstrap added to the frontend

--- admin-app/src/containers/category/Category.js
+++ admin-app/src/containers/category/Category.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useEffect,useState } from "react";
+import { Col, Container, Row,Modal,Button} from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCategory } from "../../actions";
 import Layout from "../../components/Layout/Layout";
@@ -8,6 +8,7 @@ export default function Category() {
 
   const category = useSelector(state => state.category);
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     dispatch(getAllCategory())
@@ -15,13 +16,17 @@ export default function Category() {
   
   },[]);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const renderCategories = (categories) => {
 
     let myCategories = [];
     for (let category of categories){
       myCategories.push(
-        <li>
+        <li key={category.name}>
           {category.name}
+          {category.children.length > 0 ? (<ul>{renderCategories(category.children)}</ul>) : null}
         </li>
       )
     }
@@ -37,7 +42,7 @@ export default function Category() {
                 <Col md={12}>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
                 <h3>Category</h3>
-                <button>Add</button>
+                <button onClick={handleShow}>Add</button>
                 </div>
                 </Col>
             </Row>
@@ -49,6 +54,22 @@ export default function Category() {
               </Col>
             </Row>
         </Container>
+
+     <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </Layout>
   );
 }
