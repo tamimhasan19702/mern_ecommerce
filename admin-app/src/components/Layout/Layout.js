a5f6774e0bc6f9c6a6250505b7db2eaf5633fbe1
tamimhasan19702 <tamimhasancu@gmail.com>
2023-06-18 22:52:25 +0600

    converting category action and created a new buildNewCategorie function in reducer file

--- admin-app/src/components/Layout/Layout.js
+++ admin-app/src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import React from "react";
 import Header from "../Header/Header";
 import { Container, Row, Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import "./Layout.css"
 
 export default function Layout(props) {
   return (
--- admin-app/src/reducers/category.reducers.js
+++ admin-app/src/reducers/category.reducers.js
@@ -8,6 +8,19 @@ const initState = {
   error: null,
 };
 
+const buildNewCategories = (categories, category) => {
+  let myCategories = [];
+
+  for(let cat of categories){
+  myCategories.push({
+    ...cat,
+    children: cat.children && cat.children.length > 0 ? buildNewCategories(cat.children, category) : []
+  })
+  }
+
+  return myCategories;
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initState, action) => {
   // eslint-disable-next-line default-case
@@ -28,6 +41,7 @@ export default (state = initState, action) => {
     case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
       state = {
         ...state,
+        categories: buildNewCategories(state.categories, action.payload.category),
         loading: false,
       };
       break;
