38fd202ae20a4b875241061de53014da4de2a70d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-07-21 11:15:05 +0600

    cart js controller revised and working

--- ecommerce_backend/src/controller/cart.js
+++ ecommerce_backend/src/controller/cart.js
@@ -8,56 +8,62 @@
 const Cart = require("../models/cart");
 
 exports.addItemToCart = (req, res) => {
+  //findining cart items with user id and returning error and cart item
   Cart.findOne({ user: req.user._id }).exec((error, cart) => {
+    //retrun if there's any error
     if (error) return res.status(400).json({ error });
+    // if cart already exists then update cart by quantity
     if (cart) {
-      // if cart already exists then update cart by quantity
+      //checking if the new added product is matched with previously added cartItems
       const product = req.body.cartItems.product;
-      const item = cart.cartItems.find(c => c.product == product);
-      let condition,update;
-      
+      const item = cart.cartItems.find((c) => c.product == product);
 
-      if(item){
-        condition = {"user": req.user._id, "cartItems.product": product}
+      //taking condition and update variable for  findOneAndUpdate method from mongoose
+      let condition, update;
+
+      //if item existed
+      if (item) {
+        //new condition
+        condition = { user: req.user._id, "cartItems.product": product };
+        //update and set the cartItems with previous properties and quantity
         update = {
-          "$set": {
-            "cartItems.$": {
+          $set: {
+            cartItems: {
               ...req.body.cartItems,
-              quantity: item.quantity + req.body.cartItems.quantity
-            }
-          }
-        }
-
-
-        
-
-      }else{
-        condition = { user: req.user._id };
-        update = {
-          "$push": {
-            "cartItems": req.body.cartItems
+              quantity: item.quantity + req.body.cartItems.quantity,
+            },
           },
-        }
+        };
+      } else {
+        //if item never existed
+        //create new condition for the cartItem
+        condition = { user: req.user._id };
+        //pushin the item's to the cartItems
+        update = {
+          $push: {
+            cartItems: req.body.cartItems,
+          },
+        };
       }
-      Cart
-      .findOneAndUpdate(condition,update)
-      .exec((error,_cart) => {
+
+      //findOneUpdate takes condition and update argument which finds and updates cartItems
+
+      Cart.findOneAndUpdate(condition, update).exec((error, _cart) => {
+        //if error found
         if (error) return res.status(400).json({ error });
-        if(_cart){
-          return res.status(201).json({cart: _cart});
+        //returned cart Items
+        if (_cart) {
+          return res.status(201).json({ cart: _cart });
         }
-      })
-     
-
-
-
+      });
     } else {
-      //if cart did not exist then create a new cart
+      //if cart did not exist then create a new cart object with userItems and add cartItems
       const cart = new Cart({
         user: req.user._id,
         cartItems: req.body.cartItems,
       });
 
+      //saving cart and reteuning the cart object as a response to the request
       cart.save((error, cart) => {
         if (error) return res.status(400).json({ error });
         if (cart) {
