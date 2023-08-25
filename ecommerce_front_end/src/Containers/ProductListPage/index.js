37a27f88f5583f47f12ae624ae0c56143031495b
tamimhasan19702 <tamimhasancu@gmail.com>
2023-08-25 20:35:26 +0600

    update ecommerce_front_end/src/Containers/ProductListPage/index.js and style.css

--- ecommerce_front_end/src/Containers/ProductListPage/index.js
+++ ecommerce_front_end/src/Containers/ProductListPage/index.js
@@ -1,3 +1,9 @@
+/**
+ * eslint-disable array-callback-return
+ *
+ * @format
+ */
+
 /** @format */
 
 import React, { useEffect } from "react";
@@ -5,6 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProductBySlug } from "../../actions";
 import Layout from "../../Components/Layout";
 import "./style.css";
+import { generatePublicUrl } from "../../urlConfig";
 
 export default function ProductListPage(props) {
   const product = useSelector((state) => state.product);
@@ -21,26 +28,31 @@ export default function ProductListPage(props) {
         return (
           <div className="card">
             <div className="cardHeader">
-              <div>Samsung mobile under 10K</div>
+              <div>{props.match.params.slug} mobile under 10K</div>
               <button>View all</button>
             </div>
 
-            <div className="productContainer">
-              <div className="productImgContainer">
-                <img
-                  src="http://localhost:3000/public/-72gp5T8A-0515605_samsung-galaxy-tab-a-2019-2-gb-32-gb-black_400.jpeg"
-                  alt=""
-                />
-              </div>
-
-              <div className="productInfo">
-                <div style={{ margin: "5px 0" }}>Samsung 4gb Phone</div>
-                <div>
-                  <span>4.3</span> &nbsp;
-                  <span>3533</span>
-                </div>
-                <div className="productPrice">500</div>
-              </div>
+            <div style={{ display: "flex" }}>
+              {product.productsByPrice[key].map((product, index) => {
+                return (
+                  <div className="productContainer">
+                    <div className="productImgContainer">
+                      <img
+                        src={generatePublicUrl(product.productPictures[0].img)}
+                        alt={product.name}
+                      />
+                    </div>
+                    <div className="productInfo">
+                      <div style={{ margin: "5px 0" }}>{product.name}</div>
+                      <div>
+                        <span>4.3</span>
+                        <span>3533</span>
+                      </div>
+                      <div className="productPrice">{product.price}</div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         );
